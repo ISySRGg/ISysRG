62f3e997384a30ce14a67b42e49dd140dc1db215
Rahmat <slow9ie@gmail.com>
2024-11-13 18:55:40 +0700

    feat: populate activity page

--- next-app/src/app/(contents)/activities/page.tsx
+++ next-app/src/app/(contents)/activities/page.tsx
@@ -1,3 +1,49 @@
-export default function Page() {
-  return <div></div>
+import Image from "next/image"
+import { client } from "@/sanity/client"
+import { allActivityQuery } from "@/sanity/queries"
+import { urlForImage } from "@/sanity/utils"
+
+import { Activity } from "@/types/sanity.types"
+import BasePage from "@/components/base-page"
+import BaseSection from "@/components/base-section"
+
+const options = { next: { revalidate: 30 } }
+
+export default async function Page() {
+  const activities = await client.fetch<Activity[]>(
+    allActivityQuery,
+    {},
+    options
+  )
+
+  return (
+    <BasePage title="Activites">
+      <BaseSection>
+        <div className="columns-2 gap-8">
+          {activities.map((activity) => (
+            <figure
+              key={activity._id}
+              className="flex break-inside-avoid flex-col py-4"
+            >
+              <Image
+                src={urlForImage(activity.image)?.url() as string}
+                alt=""
+                width={600}
+                height={600}
+                className="w-full rounded object-cover"
+              />
+              <figcaption className="mt-2">
+                <h3 className="text-xl font-medium">{activity.title}</h3>
+                <article className="prose">
+                  <p className="text-muted-foreground">
+                    {activity.description}
+                  </p>
+                </article>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
+      </BaseSection>
+    </BasePage>
+  )
 }
