fa3d7990ae59f0f3719e064b5be23cd86ff662f2
Rahmat <slow9ie@gmail.com>
2024-11-17 20:21:41 +0700

    fix: improve ui

--- next-app/src/components/layout/header.tsx
+++ next-app/src/components/layout/header.tsx
@@ -98,18 +98,18 @@ export default async function Header() {
     <header className="border-y-2 border-white/10">
       <div className="container flex h-14 items-center justify-between sm:h-fit">
         <div className="flex items-center divide-x divide-neutral-500">
-          <div className="pr-4">
+          <div className="pr-3 sm:pr-4">
             <Link href="https://unsri.ac.id">
               <Image
                 src="/assets/images/unsri.png"
                 alt="Unsri"
                 width={44}
                 height={44}
-                className="object-contain"
+                className="size-9 object-contain sm:size-12"
               />
             </Link>
           </div>
-          <div className="pl-4">
+          <div className="pl-3 sm:pl-4">
             <ISysLogo />
           </div>
         </div>
