f93e8e1fbb0ffadde57937e1bf1a47c1271af1fe
Rahmat <slow9ie@gmail.com>
2024-11-28 22:59:14 +0700

    fix: sanity looping request

--- next-app/src/app/not-found.tsx
+++ next-app/src/app/not-found.tsx
@@ -1,15 +1,6 @@
-"use client"
-
-import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { ArrowLeft } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
 import Header from "@/components/layout/header"
 
 export default function NotFound() {
-  const router = useRouter()
-
   return (
     <>
       <div className="bg-neutral-900 text-white">
@@ -21,14 +12,6 @@ export default function NotFound() {
         <article className="prose prose-sm sm:prose-base">
           <p>Sorry, the page you are looking doesn&apos;t exist.</p>
         </article>
-        <div className="mt-10 flex flex-col gap-4 sm:flex-row">
-          <Button variant="secondary" onClick={() => router.back()}>
-            <ArrowLeft /> Go back
-          </Button>
-          <Button asChild>
-            <Link href="/">Take me home</Link>
-          </Button>
-        </div>
       </main>
     </>
   )
