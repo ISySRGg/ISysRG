a580b27d9ddb1eea58ec3fca470449147a7c5305
Rahmat <slow9ie@gmail.com>
2024-12-07 21:34:31 +0700

    ui: fix logo size

--- next-app/src/components/isys-logo.tsx
+++ next-app/src/components/isys-logo.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 
 export default function ISysLogo() {
   return (
-    <Link href="/" className="group flex items-center gap-1.5 sm:gap-1">
+    <Link href="/" className="group flex items-center gap-0.5 sm:gap-1">
       <Image
         src="/isysrg.svg"
         alt="ISysRG"
@@ -15,10 +15,10 @@ export default function ISysLogo() {
         className="size-7 transition-all group-hover:grayscale sm:size-9"
       />
       <p className={cn(poppins.className, "flex flex-col")}>
-        <span className="mt-1 text-lg font-semibold italic sm:text-4xl">
+        <span className="mt-1 text-2xl font-semibold italic sm:text-4xl">
           ISys
         </span>
-        <span className="-mt-1.5 ml-12 text-[8px] font-light uppercase">
+        <span className="-mt-1.5 ml-8 text-[6px] font-light uppercase sm:ml-12 sm:text-[8px]">
           Research Group
         </span>
       </p>
