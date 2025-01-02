e707dc40533b993bf6ca583978972ce5d8bed29f
Rahmat <slow9ie@gmail.com>
2025-01-02 15:39:27 +0700

    fix: statcounter not working

--- next-app/src/app/statcounter.tsx
+++ next-app/src/app/statcounter.tsx
@@ -1,25 +1,40 @@
 "use client"
 
-import Script from "next/script"
-
 // FIXME: Failed to execute 'write' on 'Document': It isn't possible to write into a document from an asynchronously-loaded external script unless it is explicitly opened.
 export default function Statcounter() {
   if (process.env.NODE_ENV === "development") return null
 
   return (
-    <Script
-      id="statcounter"
-      strategy="beforeInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-    var sc_project=13071556;  
-    var sc_invisible=0; 
-    var sc_security="5359c3b5";  
-    var scJsHost = "https://";
-    document.write("<sc"+"ript type='text/javascript' src='" + scJsHost +
-    "statcounter.com/counter/counter.js'></"+"script>");
-  `,
-      }}
-    />
+    //   <Script
+    //     id="statcounter"
+    //     strategy="beforeInteractive"
+    //     dangerouslySetInnerHTML={{
+    //       __html: `
+    //   var sc_project=13071556;
+    //   var sc_invisible=0;
+    //   var sc_security="5359c3b5";
+    //   var scJsHost = "https://";
+    //   document.write("<sc"+"ript type='text/javascript' src='" + scJsHost +
+    //   "statcounter.com/counter/counter.js'></"+"script>");
+    // `,
+    //     }}
+    //   />
+
+    <>
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+        var sc_project=13071556;  
+                  var sc_invisible=0; 
+                  var sc_security="5359c3b5";  
+                  var scJsHost = "https://";
+                  document.write("<script type='text/javascript' src='" +
+                  scJsHost +
+                  "statcounter.com/counter/counter.js'></script>");
+                  `,
+        }}
+      />
+    </>
   )
 }
