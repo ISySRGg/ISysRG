280908d0509996c2a78eb062171edd4f9f8bb08a
Rahmat <slow9ie@gmail.com>
2025-01-13 11:47:09 +0700

    feat: add register batch 7 link

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -17,6 +17,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/pendaftaran-batch7",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSeBki719pOSn-eyZdzwQ9WcEkSpfY3UlwtRwTZ4c6QvFVG_pg/viewform?usp=sharing",
+        permanent: false,
+        basePath: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
