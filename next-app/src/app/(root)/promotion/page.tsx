5180ef2abf5ad6111be8c02be676d67dc43f413d
unknown <adeirianisapitri@gmail.com>
2026-03-09 10:53:25 +0700

    fix

--- next-app/src/app/(root)/promotion/page.tsx
+++ next-app/src/app/(root)/promotion/page.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image"
-import Link from "next/link"
-import { ArrowRight } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
 
 export default function Page() {
   const galleryImages = [
